cd367e2cc1717e51ef286d784cd4c3e39d9705d1
AlexM4rcelli <6780@holbertonstudents.com>
2024-01-22 13:50:55 -0300

    Update 1-writeme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
-  (err) ? (() => { throw err; }) : (() => { console.log(data); });
+  if (err) {
+    throw err;
+  } else {
+    console.log(data);
+  }
 });
--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const fileContent = process.argv[3];
 
-fs.writeFile(filePath, fileContent, 'utf-8', (err) => {
-  if (err) throw err;
+fs.writeFile(filePath, fileContent,'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
 });
