db439a23f72f0178caf74403051aa5fc9788deab
AlexM4rcelli <6780@holbertonstudents.com>
2024-01-22 13:40:57 -0300

    Update 1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,6 +5,6 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const fileContent = process.argv[3];
 
-fs.writeFile(filePath, 'utf-8', fileContent, (err) => {
+fs.writeFile(filePath, fileContent, 'utf-8', (err) => {
   if (err) throw err;
 });
